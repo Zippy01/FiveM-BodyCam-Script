8f798e807ce38b7041f4097173a8f5d672363dd3
Zippy OFC <72061397+Zippy01@users.noreply.github.com>
2024-08-06 03:11:32 -0400

    Source code

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zippy OFC'
+description 'Bodycam script'
+version '1.0.0'
+
+
+-- Please dont try to steal my work, BUT feel free to buy or obtain more free work from me at https://zippy-productions.com
+
+
+client_script 'client.lua'
+
+files {
+    'sounds/beep.ogg',
+    'images/bodycam.png',
+    'index.html'
+}
+
+ui_page 'index.html'
